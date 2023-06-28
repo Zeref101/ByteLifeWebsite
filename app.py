ad2f71dc0d9cf16fac80d673318ab76b7e683638
bytelifeGH <tolasarianaman200@gmail.com>
2023-06-28 23:47:24 +0530

    Revert "we added https"
    This reverts commit 08d607efc83c13158b4e8549c7d040c871a9412e.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import ssl
 
 app = Flask(__name__)
 
@@ -9,6 +8,4 @@ def index():
 
 
 if __name__ == "__main__":
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    ssl_context.load_cert_chain('../secrets/certificate.crt', '../secrets/private.key')
-    app.run(ssl_context=ssl_context, host="0.0.0.0")
+    	app.run(host="0.0.0.0")
