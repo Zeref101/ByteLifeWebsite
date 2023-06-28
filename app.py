08d607efc83c13158b4e8549c7d040c871a9412e
bytelifeGH <tolasarianaman200@gmail.com>
2023-06-28 23:37:08 +0530

    we added https
    truly this time

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import ssl
 
 app = Flask(__name__)
 
@@ -8,4 +9,6 @@ def index():
 
 
 if __name__ == "__main__":
-    	app.run(host="0.0.0.0")
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain('../secrets/certificate.crt', '../secrets/private.key')
+    app.run(ssl_context=ssl_context, host="0.0.0.0")
